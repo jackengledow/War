69b3275627d885794967caceccac1c58c026854b
Jack <jmengledow@indy.rr.com>
2015-02-10 19:14:58 -0500

    almost done debugging draw/shuffle function

--- War.js
+++ War.js
@@ -5,9 +5,9 @@ var A = 14;
 var j;
 var o;
 var deck = [ A, A, A, A, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, J, J, J, J, Q, Q, Q, Q, K, K, K, K];
-var i;
+var i = 0;
 
-var draw = function(){
+var shuffle = function(){
 	var currentIndex = deck.length, temporaryValue, randomIndex ;
 	while (0 !== currentIndex) {
 		randomIndex = Math.floor(Math.random() * currentIndex);
@@ -17,11 +17,17 @@ var draw = function(){
 		deck[randomIndex] = temporaryValue;
 	}
 	var userDeck = deck.slice(0, 26);
+	var computerDeck = deck.slice(26, 53);
+}
+shuffle(deck);
+var draw = function(){
+	var userDeck = deck.slice(0, 26);
 	console.log(userDeck);
 	console.log(userDeck[0]);
 	var computerDeck = deck.slice(26, 53);
 	console.log(computerDeck);
 	console.log(computerDeck[0]);
+	i++;
 	if (computerDeck[i] > userDeck[i]){
 		console.log("lose");
 	}
@@ -31,7 +37,6 @@ var draw = function(){
 	else if (computerDeck[i] === userDeck[i]){
 		console.log("tie");
 	}
-	i++;
 }
 draw(deck);
 
@@ -44,7 +49,7 @@ draw(deck);
 //Alec is working :>
 
 var restart = function() {
-	shuffle(deck);
+	draw(deck);
 	document.getElementById("default").style.display = "block";
 	document.getElementById("win").style.display = "none";
 	document.getElementById("lose").style.display = "none";
