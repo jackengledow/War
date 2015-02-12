728b9602ff213365919d3e7509e1d17afe6a0071
Jack <jmengledow@indy.rr.com>
2015-02-12 18:25:49 -0500

    added cards to js

--- War.js
+++ War.js
@@ -1,60 +1,69 @@
-var twoh = 2;
-var twos = 2;
-var twoc = 2;
-var twod = 2;
-var threeh = 3;
-var threes = 3;
-var threec = 3;
-var threed = 3;
-var fourh = 4;
-var fours = 4;
-var fourc = 4;
-var fours = 4;
-var fived = 5;
-var fivec = 5;
-var fives = 5;
-var fiveh = 5;
-var sixd = 6;
-var sixc = 6;
-var sixs = 6;
-var sixh = 6;
-var sevend = 7;
-var sevenc = 7;
-var sevens = 7;
-var sevenh = 7;
-var eights = 8;
-var eightd = 8;
-var eightc = 8;
-var eighth = 8;
-var nined = 9;
-var ninec = 9;
-var nines = 9;
-var nineh = 9;
-var tenc = 10;
-var tens = 10;
-var tend = 10;
-var tenh = 10;
-var Jc = 11;
-var Jd = 11;
-var Js = 11;
-var Jh = 11;
-var Qd = 12;
-var Qc = 12;
-var Qs = 12;
-var Qh = 12;
-var Kh = 13;
-var Kc = 13;
-var Kd = 13;
-var Ks = 13;
-var Ad = 14;
-var As = 14;
-var Ac = 14;
-var Ah = 14;
-var deck =  [Ac, Ad, Ah, As, twoh, twos, twoc, twod, threeh, threes, threec, threed, fourh, fours, fourc, fours, fived, fivec, fives, fiveh, sixd, sixc, sixs, sixh, sevend, sevenc ,sevens, sevenh, eights, eightd, eightc, eighth, nined, ninec, nines, nineh, tenc, tens, tend, tenh, Jc, Jd, Jh, Js, Qs, Qd, Qh, Qc, Kh, Kc, Ks, Kd];
+var twoh = 2.0;
+var twos = 2.1;
+var twoc = 2.2;
+var twod = 2.3;
+var threeh = 3.0;
+var threes = 3.1;
+var threec = 3.2;
+var threed = 3.3;
+var fourh = 4.0;
+var fours = 4.1;
+var fourc = 4.2;
+var fourd = 4.3;
+var fived = 5.0;
+var fivec = 5.1;
+var fives = 5.2;
+var fiveh = 5.3;
+var sixd = 6.0;
+var sixc = 6.1;
+var sixs = 6.2;
+var sixh = 6.3;
+var sevend = 7.0;
+var sevenc = 7.1;
+var sevens = 7.2;
+var sevenh = 7.3;
+var eights = 8.0;
+var eightd = 8.1;
+var eightc = 8.2;
+var eighth = 8.3;
+var nined = 9.0;
+var ninec = 9.1;
+var nines = 9.2;
+var nineh = 9.3;
+var tenc = 10.0;
+var tens = 10.1;
+var tend = 10.2;
+var tenh = 10.3;
+var Jc = 11.0;
+var Jd = 11.1;
+var Js = 11.2;
+var Jh = 11.3;
+var Qd = 12.0;
+var Qc = 12.1;
+var Qs = 12.2;
+var Qh = 12.3;
+var Kh = 13.0;
+var Kc = 13.1;
+var Kd = 13.2;
+var Ks = 13.3;
+var Ad = 14.0;
+var As = 14.1;
+var Ac = 14.2;
+var Ah = 14.3;
+var deck =  [Ac, Ad, Ah, As, twoh, twos, twoc, twod, threeh, threes, threec, threed, fourh, fours, fourc, fours, fived, fivec, fives, fiveh, sixd, sixc, sixs, sixh, sevend, sevenc, sevens, sevenh, eights, eightd, eightc, eighth, nined, ninec, nines, nineh, tenc, tens, tend, tenh, Jc, Jd, Jh, Js, Qs, Qd, Qh, Qc, Kh, Kc, Ks, Kd];
+console.log(deck.length);
+var number = ["screwDat", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "J", "Q", "K", "A"];
+var suit = ["c", "d", "s", "h"];
 var i = 0;
 
+var firstRun = true;
+var computerDeck;
+var userDeck;
+
 var shuffle = function(){
-	var currentIndex = deck.length, temporaryValue, randomIndex ;
+	var currentIndex = deck.length;
+	var temporaryValue;
+	var randomIndex;
 	while (0 !== currentIndex) {
 		randomIndex = Math.floor(Math.random() * currentIndex);
 		currentIndex -= 1;
@@ -62,43 +71,59 @@ var shuffle = function(){
 		deck[currentIndex] = deck[randomIndex];
 		deck[randomIndex] = temporaryValue;
 	}
-	var userDeck = deck.slice(0, 26);
-	var computerDeck = deck.slice(26, 53);
 }
-shuffle(deck);
 
 var draw = function(){
-	var userDeck = deck.slice(0, 26);
-	console.log(userDeck);
-	console.log(userDeck[i]);
-	var computerDeck = deck.slice(26, 53);
-	console.log(computerDeck);
-	console.log(computerDeck[i]);
-	if (computerDeck[i] > userDeck[i]){
-		console.log("lose");
-		document.getElementById("whole").style.backgroundColor = "red";
-		document.getElementById("lose").style.display = "block";
-		document.getElementById("win").style.display = "none";
-		document.getElementById("tie").style.display = "none";
-		document.getElementById("default").style.display = "none";
+	if (!firstRun){
+
+		document.getElementById("computerCard").className = "cardDiv " + number[Math.floor(computerDeck[i])] + suit[0];
+		document.getElementById("userCard").className = "cardDiv " + number[Math.floor(userDeck[i])] + suit[0];
+
+		if (Math.floor(computerDeck[i]) > Math.floor(userDeck[i]) || ((Math.floor(computerDeck[i]) == 2) && (Math.floor(userDeck[i]) == 14))){
+			console.log("lose");
+			document.getElementById("whole").style.backgroundColor = "red";
+			document.getElementById("lose").style.display = "block";
+			document.getElementById("win").style.display = "none";
+			document.getElementById("tie").style.display = "none";
+			document.getElementById("default").style.display = "none";
+			computerDeck.push(userDeck[i]);
+			userDeck.shift();
+			console.log("userDeck After:\nLength is: " + userDeck.length + "\nContents: " + userDeck);
+			console.log("computerDeck After:\nLength is: " + computerDeck.length + "\nContents: " + computerDeck);
+		}
+		else if (Math.floor(computerDeck[i]) < Math.floor(userDeck[i]) || ((Math.floor(computerDeck[i]) == 14) && (Math.floor(userDeck[i]) == 2))){
+			console.log("win");
+			document.getElementById("whole").style.backgroundColor = "green";
+			document.getElementById("win").style.display = "block";
+			document.getElementById("lose").style.display = "none";
+			document.getElementById("tie").style.display = "none";
+			document.getElementById("default").style.display = "none";
+			userDeck.push(computerDeck[i]);
+			computerDeck.shift();
+			console.log("userDeck After:\nLength is: " + userDeck.length + "\nContents: " + userDeck);
+			console.log("computerDeck After:\nLength is: " + computerDeck.length + "\nContents: " + computerDeck);
+		}
+		else if (Math.floor(computerDeck[i]) == Math.floor(userDeck[i])){
+			console.log("tie");
+			document.getElementById("whole").style.backgroundColor = "black";
+			document.getElementById("tie").style.display = "block";
+			document.getElementById("win").style.display = "none";
+			document.getElementById("lose").style.display = "none";
+			document.getElementById("default").style.display = "none";
+			console.log("userDeck After:\nLength is: " + userDeck.length + "\nContents: " + userDeck);
+			console.log("computerDeck After:\nLength is: " + computerDeck.length + "\nContents: " + computerDeck);
+			firstRun = false;
+		}
 	}
-	else if (computerDeck[i] < userDeck[i]){
-		console.log("win");
-		document.getElementById("whole").style.backgroundColor = "green";
-		document.getElementById("win").style.display = "block";
-		document.getElementById("lose").style.display = "none";
-		document.getElementById("tie").style.display = "none";
-		document.getElementById("default").style.display = "none";
-		userDeck.push(computerDeck[i]);
-		console.log(userDeck);
-	}
-	else if (computerDeck[i] = userDeck[i]){
-		console.log("tie");
-		document.getElementById("whole").style.backgroundColor = "black";
-		document.getElementById("tie").style.display = "block";
-		document.getElementById("win").style.display = "none";
-		document.getElementById("lose").style.display = "none";
-		document.getElementById("default").style.display = "none";
+	else {
+		shuffle(deck);
+		userDeck = deck.slice(0, 26);
+		console.log("userDeck Before:\nLength is: " + userDeck.length + "\nContents: " + userDeck);
+		console.log(userDeck[i]);
+		computerDeck = deck.slice(26, 53);
+		console.log("computerDeck Before:\nLength is: " + computerDeck.length + "\nContents: " + computerDeck);
+		console.log(computerDeck[i]);
+		firstRun = false;
 	}
 	i++;
 }
@@ -116,5 +141,6 @@ var restart = function() {
 	document.getElementById("tie").style.display = "none";
 	document.getElementById("whole").style.backgroundColor = "#000066";
 	i = 0;
+	firstRun = true;
 }
 
